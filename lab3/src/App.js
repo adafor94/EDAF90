639ec6274034f49180b02265999789aa879e3c4c
Adam Forsberg <adamforsberg94@gmail.com>
2022-02-03 11:09:16 +0100

    added some routes

--- lab3/src/App.js
+++ lab3/src/App.js
@@ -1,65 +1,85 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import inventory from './inventory.ES6';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import inventory from "./inventory.ES6";
 
-import {Component} from 'react';
-import ComposeSalad from './ComposeSalad';
-import ViewCart from './ViewCart';
-import { Link } from "react-router-dom"
+import { Component } from "react";
+import ComposeSalad from "./ComposeSalad";
+import ViewCart from "./ViewCart";
+import { Link, Routes, Route } from "react-router-dom";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { shoppingCart : []};
+    this.state = { shoppingCart: [] };
     this.addSaladToCart = this.addSaladToCart.bind(this);
   }
 
-  addSaladToCart(salad) {    
+  addSaladToCart(salad) {
     this.setState((state, props) => {
-      return {shoppingCart : [...state.shoppingCart,salad]};
-    })
+      return { shoppingCart: [...state.shoppingCart, salad] };
+    });
   }
 
-  render () {
+  render() {
     return (
       <div className="container py-4">
-        <Header />  
+        <Header />
         {
-        // Header()  Why not like this? 
+          // Header()  Why not like this?
         }
 
-        <Navbar/> 
+        <Navbar />
 
-        {this.renderPageContent()}
-        <Footer /> 
+        {this.renderRouter()}
+        <Footer />
       </div>
     );
-    }
+  }
 
-  renderPageContent(){
-    return(
-      <div> 
-        <ComposeSalad inventory={inventory} addSaladToCart = {this.addSaladToCart} />    
-        <ViewCart order={this.state.shoppingCart} />  
-      </div>
+  renderRouter() {
+    return (
+      <Routes>
+        <Route
+          path="/"
+          element={<div> Välkommen till min fina salladsbar </div>}
+        />
+        <Route
+          path="/compose-salad"
+          element={
+            <ComposeSalad
+              inventory={inventory}
+              addSaladToCart={this.addSaladToCart}
+            />
+          }
+        />
+        <Route
+          path="/view-order"
+          element={<ViewCart order={this.state.shoppingCart} />}
+        />
+        <Route
+          path="/view-order"
+          element={<ViewCart order={this.state.shoppingCart} />}
+        />
+        <Route path="*" element={<div>Sorry, page not found</div>} />
+      </Routes>
     );
   }
 }
 
 function Footer() {
-  return(
+  return (
     <footer className="pt-3 mt-4 text-muted border-top">
       EDAF90 - webprogrammering
     </footer>
-  )
+  );
 }
 
 function Header() {
-  return(
+  return (
     <header className="pb-3 mb-4 border-bottom">
       <span className="fs-4">Min egen salladsbar</span>
     </header>
-  )
+  );
 }
 
 function Navbar() {
@@ -67,16 +87,16 @@ function Navbar() {
     <ul className="nav nav-tabs">
       <li className="nav-item">
         <Link className="nav-link" to="/compose-salad">
-        Komponera en sallad
+          Komponera en sallad
         </Link>
       </li>
-      <li className="nav-item"> 
-        <Link className="nav-link" to="/view-order"> 
-        Se din order
+      <li className="nav-item">
+        <Link className="nav-link" to="/view-order">
+          Se din order
         </Link>
       </li>
-    </ul>);
+    </ul>
+  );
 }
 
-  
 export default App;
