a8f07b3adcff3660bc5cdc74f8e48efb89f0181a
Adam Forsberg <adamforsberg94@gmail.com>
2022-02-03 10:47:32 +0100

    here you go

--- lab3/src/App.js
+++ lab3/src/App.js
@@ -5,36 +5,78 @@ import inventory from './inventory.ES6';
 import {Component} from 'react';
 import ComposeSalad from './ComposeSalad';
 import ViewCart from './ViewCart';
+import { Link } from "react-router-dom"
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = { shoppingCart : []};
-
     this.addSaladToCart = this.addSaladToCart.bind(this);
   }
 
-    addSaladToCart(salad) {    
-      this.setState((state, props) => {
-        return {shoppingCart : [...state.shoppingCart,salad]};
-      })
-    }
+  addSaladToCart(salad) {    
+    this.setState((state, props) => {
+      return {shoppingCart : [...state.shoppingCart,salad]};
+    })
+  }
 
   render () {
     return (
       <div className="container py-4">
-      <header className="pb-3 mb-4 border-bottom">
-        <span className="fs-4">Min egen salladsbar</span>
-      </header>
+        <Header />  
+        {
+        // Header()  Why not like this? 
+        }
 
-      <ComposeSalad inventory={inventory} addSaladToCart = {this.addSaladToCart} />    
-      <ViewCart order={this.state.shoppingCart} />  
-      <footer className="pt-3 mt-4 text-muted border-top">
-        EDAF90 - webprogrammering
-      </footer>
-    </div>
+        <Navbar/> 
+
+        {this.renderPageContent()}
+        <Footer /> 
+      </div>
     );
     }
+
+  renderPageContent(){
+    return(
+      <div> 
+        <ComposeSalad inventory={inventory} addSaladToCart = {this.addSaladToCart} />    
+        <ViewCart order={this.state.shoppingCart} />  
+      </div>
+    );
+  }
 }
 
+function Footer() {
+  return(
+    <footer className="pt-3 mt-4 text-muted border-top">
+      EDAF90 - webprogrammering
+    </footer>
+  )
+}
+
+function Header() {
+  return(
+    <header className="pb-3 mb-4 border-bottom">
+      <span className="fs-4">Min egen salladsbar</span>
+    </header>
+  )
+}
+
+function Navbar() {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <Link className="nav-link" to="/compose-salad">
+        Komponera en sallad
+        </Link>
+      </li>
+      <li className="nav-item"> 
+        <Link className="nav-link" to="/view-order"> 
+        Se din order
+        </Link>
+      </li>
+    </ul>);
+}
+
+  
 export default App;
